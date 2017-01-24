0796d0dd3324976cc727156a9be367f48407bd9a
Olivier Crete <olivier.crete@orange.com>
2017-01-24 17:32:11 +0100

    Update help

--- my_yams.php
+++ my_yams.php
@@ -119,7 +119,7 @@ class Yams {
 
 	public static function displayHelp () {
 		echo join("\n", ['USAGE', 'php my_yams.php d1 d2 d3 d4 d5 c', '', 'DESCRIPTION', 'd1 la valeur de premier dé', 'd2 la valeur de deuxième dé',
-		'd3 la valeur de troisième dé', 'd4 la valeur de quatrième dé', 'd5 la valeur de cinquième dé', 'd6 la valeur de sixième dé', 'c la combinaison souhaitée', '', 'combinaison: paire, brelan, carre, yams, full, suite', 'exemple:', 'php my_yams.php 1 2 3 2 1 brelan_2', 'php my_yams.php 4 5 4 3 1 full_4_3', 'php my_yams.php 2 3 3 5 4 suite_6']) . "\n";
+		'd3 la valeur de troisième dé', 'd4 la valeur de quatrième dé', 'd5 la valeur de cinquième dé', 'd6 la valeur de sixième dé', 'c la combinaison souhaitée', '', 'combinaison: paire, brelan, carre, yams, full, suite', 'EXEMPLE:', 'php my_yams.php 1 2 3 2 1 brelan_2', 'php my_yams.php 4 5 4 3 1 full_4_3', 'php my_yams.php 2 3 3 5 4 suite_6']) . "\n";
 	}
 
 }
